ee67c7b27160ed3e9df0cab40f2f5b0317069e7f
Lorinda <lorinda@users.noreply.github.com>
2017-02-28 17:43:55 -0500

    Add a simple bootstrap navigation menu

--- inc/header.php
+++ inc/header.php
@@ -1,11 +1,24 @@
 <!DOCTYPE html>
 <html>
     <head>
+    	<!-- Bootstrap 3.3.7 -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <title><?php echo $pageTitle; ?></title>
     </head>
     <body>
         <header>
-            <h1><?php echo $pageTitle; ?></h1>
+	        <nav class="navbar navbar-default">
+				<div class="container-fluid">
+					<div class="navbar-header">
+				    	<a class="navbar-brand" href="#"><?php echo $pageTitle; ?></a>
+				    </div>
+				    <ul class="nav navbar-nav">
+				    	<li class="active"><a href="#">Home</a></li>
+				      	<li><a href="#">Recipes</a></li>
+				      	<li><a href="#">Ingredients</a></li>
+				      	<li><a href="#">Report</a></li>
+				    </ul>
+				</div>
+			</nav>    
         </header>
-    </body>
-</html>
+     
