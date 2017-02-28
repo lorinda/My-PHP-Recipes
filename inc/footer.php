1cf4867ebc613d144dc1d14dfe50f5e7052e7b3d
Lorinda <lorinda@users.noreply.github.com>
2017-02-28 17:43:29 -0500

    Add footer file with copyright year

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+        <footer>
+            <p><?php echo "(c) ".date('Y')." My Blue Apron Meals by Lorinda Peoples"; ?></p>
+            <p><i><?php echo "(c) Blue Apron is a registered trademark of Blue Apron, 
+            LLC which grants a limited right to create text hyperlinks to the Blue Apron 
+            site for noncommercial purposes."; ?></i></p>
+        </footer>
+    </body>
+</html>
