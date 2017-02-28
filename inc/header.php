3c4d1c9c7f9ea4ba363695a9f06b3ccf77f9e597
Lorinda <lorinda@users.noreply.github.com>
2017-02-28 16:35:44 -0500

    Initial commit

---
+++ inc/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo $pageTitle; ?></title>
+    </head>
+    <body>
+        <header>
+            <h1><?php echo $pageTitle; ?></h1>
+        </header>
+    </body>
+</html>
