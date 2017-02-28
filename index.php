15acf6a823d3a72996f1e7cf1821dd3cad45963d
Lorinda <lorinda@users.noreply.github.com>
2017-02-28 17:42:59 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$pageTitle = "My Blue Apron Meals";
+include 'inc/header.php';
+?>
+<h1>Welcome to My Blue Apron Meals</h1>
+<div class="description">
+    <p>Blue Apron is a service that delivers fresh ingredients and recipes to subscribers. I began using this service in January 2016.  I have collected a few of the recipes* I have cooked on this site searchable by ingredient and recipe name.  
+    </p>
+    <p>*All recipes link to those listed on blueapron.com.
+    </p>
+</div>
+    
+
+<?php include 'inc/footer.php'; ?>
