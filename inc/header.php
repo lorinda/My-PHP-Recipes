80882538a9e0c0cf9ab1446ee3703ee6a0e96eef
Lorinda <lorinda@users.noreply.github.com>
2017-03-06 13:46:30 -0500

    Add css links

--- inc/header.php
+++ inc/header.php
@@ -3,6 +3,7 @@
     <head>
     	<!-- Bootstrap 3.3.7 -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="../css/style.css">
         <title><?php echo $pageTitle; ?></title>
     </head>
     <body>
@@ -10,11 +11,11 @@
 	        <nav class="navbar navbar-default">
 				<div class="container-fluid">
 					<div class="navbar-header">
-				    	<a class="navbar-brand" href="#"><?php echo $pageTitle; ?></a>
+				    	<a class="navbar-brand" href="../index.php"><?php echo $pageTitle; ?></a>
 				    </div>
 				    <ul class="nav navbar-nav">
-				    	<li class="active"><a href="#">Home</a></li>
-				      	<li><a href="#">Recipes</a></li>
+				    	<li class="active"><a href="../index.php">Home</a></li>
+				      	<li><a href="../recipe.php">Recipes</a></li>
 				      	<li><a href="#">Ingredients</a></li>
 				      	<li><a href="#">Report</a></li>
 				    </ul>
