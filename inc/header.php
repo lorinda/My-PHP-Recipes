5f0648d1be399e6aff54184c4dba5b02dd9016c3
Lorinda <lorinda@users.noreply.github.com>
2017-03-06 17:27:47 -0500

    Add navbar-toggle collapsed class for mobile menu

--- inc/header.php
+++ inc/header.php
@@ -1,24 +1,43 @@
+<?php
+$siteTitle = 'My Blue Apron Meals';
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
     	<!-- Bootstrap 3.3.7 -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="../css/style.css">
-        <title><?php echo $pageTitle; ?></title>
+        <title><?php echo $siteTitle; ?></title>
     </head>
     <body>
         <header>
 	        <nav class="navbar navbar-default">
 				<div class="container-fluid">
-					<div class="navbar-header">
-				    	<a class="navbar-brand" href="../index.php"><?php echo $pageTitle; ?></a>
+					
+                    
+                    <div class="navbar-header">
+				    	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="../index.php"><?php echo $siteTitle; ?></a>
 				    </div>
-				    <ul class="nav navbar-nav">
-				    	<li class="active"><a href="../index.php">Home</a></li>
-				      	<li><a href="../recipe.php">Recipes</a></li>
-				      	<li><a href="#">Ingredients</a></li>
-				      	<li><a href="#">Report</a></li>
-				    </ul>
+                    
+				    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="../index.php">Home</a></li>
+                            <li><a href="../recipe.php">Recipes</a></li>
+                            <li><a href="#">Ingredients</a></li>
+                            <li><a href="#">Report</a></li>
+                        </ul>
+                    </div>
 				</div>
 			</nav>    
         </header>
