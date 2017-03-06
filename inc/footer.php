0c4fe9bc6731ec753d96052a6d7adfedc14e0fee
Lorinda <lorinda@users.noreply.github.com>
2017-03-06 17:28:45 -0500

    Add jQuery and Bootstrap JavaScript script src's for mobile navigation

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,4 @@
+        
         <div id="footer">
             <footer class="footer">
                 <p><?php echo "(c) ".date('Y')." My Blue Apron Meals by Lorinda Peoples"; ?></p>
@@ -6,5 +7,10 @@
                 site for noncommercial purposes."; ?></i></p>
             </footer>
         </div>
+    <!--jQuery -->
+    <script src="http://code.jquery.com/jquery-3.1.1.min.js" type="text/javascript"></script>
+    <!-- Bootstrap JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script>
+
     </body>
 </html>
