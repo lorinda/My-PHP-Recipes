3b246e4a8767a373647d165bd4c66bc436665756
Lorinda <lorinda@users.noreply.github.com>
2017-03-06 13:45:03 -0500

    Connect to database

--- inc/connection.php
+++ inc/connection.php
@@ -6,9 +6,6 @@ $dbase = 'my_apron';
 try{
     $db = new PDO('mysql:host=localhost;dbname='.$dbase, $user, $pd);
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    $sql = 'SELECT * FROM recipe';
-    $results = $db->query($sql);
-    var_dump($results->fetchAll(PDO::FETCH_ASSOC));
 }catch(Exception $e){
     echo 'Unable to Connect';
     echo $e->getMessage();
