1e49d4fc1269e8e9154fe6a6464dc207a85fe18e
Lorinda <lorinda@users.noreply.github.com>
2017-03-06 13:47:33 -0500

    Add <div class='jumbotron well'>

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
 $pageTitle = "My Blue Apron Meals";
 include 'inc/header.php';
 ?>
-<h1>Welcome to My Blue Apron Meals</h1>
-<div class="description">
-    <p>Blue Apron is a service that delivers fresh ingredients and recipes to subscribers. I began using this service in January 2016.  I have collected a few of the recipes* I have cooked on this site searchable by ingredient and recipe name.  
-    </p>
-    <p>*All recipes link to those listed on blueapron.com.
-    </p>
-</div>
-    
+<div class="jumbotron well">
+    <h1>Welcome to My Blue Apron Meals</h1>
+    <div class="description">
+        <p>Blue Apron is a service that delivers fresh ingredients and recipes to subscribers. I began using this service in January 2016.  I have collected a few of the recipes* I have cooked on this site searchable by ingredient and recipe name.  
+        </p>
+        <p>*All recipes link to those listed on blueapron.com.
+        </p>
+    </div>
+</div>    
 
 <?php include 'inc/footer.php'; ?>
