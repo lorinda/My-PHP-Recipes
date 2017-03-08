c3d498273151ed8355b5cf32de6f7a62b01ab317
Lorinda <lorinda@users.noreply.github.com>
2017-03-07 21:00:21 -0500

    Add basic form

---
+++ add-recipe.php
@@ -0,0 +1,54 @@
+<?php
+include 'inc/connection.php';
+include 'inc/functions.php';
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    var_dump($_POST);
+}
+include 'inc/header.php';
+
+
+?>
+<div class="jumbotron well">
+    <h1>Add a Recipe</h1>
+    <form action="#" method="post">
+        <table>
+            <tr>
+                <td>
+                    <label for="title">Recipe Title (required)</label>
+                </td>
+                <td>
+                    <input type="text" name="title" id="title" size="40" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="subtitle">Recipe Subtitle (required)</label>
+                </td>
+                <td>
+                    <input type="text" name="subtitle" id="subtitle" size="40" />
+                </td>
+                
+            </tr>
+            <tr>
+                <td>
+                    <label for="cooked_on">Date Cooked (MM/DD/YYYY)</label>
+                </td>
+                <td>
+                    <input type="text" name="cooked_on" id="cooked_on" size="10" />
+                </td>
+            </tr>
+        </table>
+        <br>
+        <p><i>Optional: Add image to /image folder</i></p>
+        <label for="img_src">Image Name (include extension .jpg, etc)</label>
+        <input type="text" name="img_src" id="img_src" size="20" />
+        <br>
+        <label for="url">Blue Apron Website Link</label>
+        <input type="text" name="url" id="url" size="40" value="https://www.blueapron.com/recipes/"/>
+        <br>
+        <input type="submit" value="Continue" />
+    </form>
+</div>
+<?php
+include 'inc/footer.php';    
+?>
