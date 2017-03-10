2deebf041ebbfa9571ad8962e94f1b7be475ecfb
Lorinda <lorinda@users.noreply.github.com>
2017-03-10 15:03:59 -0500

    Delete white space

--- recipe.php
+++ recipe.php
@@ -3,34 +3,24 @@ include 'inc/functions.php';
 
 if(isset($_GET['id'])){
     $id = $_GET['id'];
-     //if(checkID($id)){
-        //if id is in recipe table
-            $recipeTitle = getRecipeTitle($id);
-            $subTitle = getRecipeSubTitle($id);
-            //display recipe for id
+        $recipeTitle = getRecipeTitle($id);
+        $subTitle = getRecipeSubTitle($id);
+            
 }else{
     $id = '';
     //display main recipe page
 }
-
-
-
 include 'inc/header.php';
 
 ?>
 <?php
 if(isset($_GET['id'])){
+//display recipe for id
 ?>
 <div class="jumbotron well">
     <div class="container">
-        <h1><?php 
-            echo $recipeTitle; 
-            ?>
-        </h1>
-        <h2><?php 
-            echo $subTitle; 
-            ?>
-        </h2>
+        <h1><?php echo $recipeTitle; ?></h1>
+        <h2><?php echo $subTitle; ?> </h2>
         <div class="container">
             <div class="col-md-4" style="padding-left: 0px;  padding-right: 0px;">
                 <img src='<?php
@@ -42,8 +32,7 @@ if(isset($_GET['id'])){
                             echo "/images/No_Image_Taken.jpg";
                         }
                     }
-
-                  ?>' class="img-responsive">
+                ?>' class="img-responsive">
             </div> <!--End col-md-4 div-->
             <div class="col-md-4">
                 <table class="table table-striped">
@@ -62,12 +51,10 @@ if(isset($_GET['id'])){
                         }
                         ?>
                     </tbody>
-                    
                 </table>
             </div> <!--End col-md-4 div-->
             <div class="col-md-4">
-                <br>
-                <b>Cooked on:</b>
+                <br><b>Cooked on:</b>
                 <?php 
                 $dateURL = getDateUrl($id);
                 foreach ($dateURL as $item){
@@ -76,21 +63,16 @@ if(isset($_GET['id'])){
                     echo "<a href='".$item['url']."' target='_blank'>"."Link to Full Recipe on Blue Apron.com."."</a>";
                 }
                 ?>
-                <br>
-
-                <br>
-
+                <br><br>
                 Update This Page Listing
             </div> <!-- End col-md-4 div-->
         </div> <!-- End container div -->
-
-
         <br>
     </div><!-- End Recipe div -->
 </div><!-- End Jumbotron Well div -->
 <?php
 }//Close if(isset)
-//////////////No ID set /////////////////
+//-----------else No ID set----------------//
 else{  
 ?>
 <div class="jumbotron well">
@@ -122,24 +104,10 @@ else{
                 }
                 ?>							
             </ul>
-<!-- DELETE LATER
-            <div class="col-md-4">
-                <img src="images/No_Image_Taken.jpg" class="img-responsive">
-            </div>
-            <div class="col-md-4">
-                <img src="images/No_Image_Taken.jpg" class="img-responsive">
-            </div>
-            <div class="col-md-4">
-                <img src="images/No_Image_Taken.jpg" class="img-responsive">
-            </div>
-DELETE LATER --> 
-            
-        </div>
-    </div>
-</div>
+        </div><!--End "row" -->
+    </div><!-- End "container" -->
+</div><!-- End "jumbotron well" -->
 
-<?php
-} //Close else
-?>
+<?php } //Close else ?>
 
 <?php include 'inc/footer.php'; ?>
