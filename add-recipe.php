926a3fce1051decdb18cbef0dd42ce4657b755a8
Lorinda <lorinda@users.noreply.github.com>
2017-03-15 14:21:03 -0400

    Add $add_recipe='selected' for active nav link

--- add-recipe.php
+++ add-recipe.php
@@ -1,13 +1,41 @@
 <?php
+$add_recipe = 'selected';
 include 'inc/connection.php';
 include 'inc/functions.php';
 include 'inc/header.php';
 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    if(isset($_POST['recipe'])){
+        if($_POST['recipe'] == 'addRecipe'){
+            $title = trim(filter_input(INPUT_POST,'title',FILTER_SANITIZE_STRING));
+            $subtitle = trim(filter_input(INPUT_POST,'subtitle',FILTER_SANITIZE_STRING));
+            $cooked_on = trim(filter_input(INPUT_POST,'cooked_on',FILTER_SANITIZE_STRING));
+            $img_src = trim(filter_input(INPUT_POST,'img_src',FILTER_SANITIZE_STRING));
+            $url = trim(filter_input(INPUT_POST,'url',FILTER_SANITIZE_STRING));
 
+            $img_src = '/images/'.$img_src;
+             //Check that Title and Subtitle were filled in
+            if(!empty($title) & !empty($subtitle)){
+                $title=ucwords($title);
+                $subtitle = ucwords($subtitle);
+                //Add Recipe to the database and re-direct
+                if(addRecipe($title, $subtitle, $cooked_on, $img_src, $url)){
+                    header('Location: add-ingredient.php');
+                }
+            }else{
+                //Display error_message after form submit
+                $error_message = 'Could Not Add Recipe. Fill Required Fields. ';
+                echo '<div class="status"><h2>';
+                echo $error_message;
+                echo '</h2></div>';
+            }
+        }//End if($_POST['recipe'] == 'addRecipe')
+    }//END if(isset($_POST['recipe']))
+}//END if($_SERVER["REQUEST_METHOD"] == "POST")
 ?>
 <div class="jumbotron well">
     <h1>Add a Recipe</h1>
-    <form action="add-ingredient.php" method="post" class="form-horizontal">
+    <form action="add-recipe.php" method="post" class="form-horizontal">
         <table>
             <tr>
                 <td>
@@ -24,7 +52,6 @@ include 'inc/header.php';
                 <td>
                     <input type="text" name="subtitle" id="subtitle" size="40" />
                 </td>
-                
             </tr>
             <tr>
                 <td>
