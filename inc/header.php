0a5eb6974969ec03a7346769d593cf61420a7a70
Lorinda <lorinda@users.noreply.github.com>
2017-03-17 19:25:06 -0400

    Add glyphicon-search to search input

--- inc/header.php
+++ inc/header.php
@@ -18,9 +18,7 @@ $siteTitle = 'My Blue Apron Meals';
         <header>
 	        <nav class="navbar navbar-default">
 				<div class="container-fluid">
-					
-                    
-                    <div class="navbar-header">
+					<div class="navbar-header">
 				    	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                             <span class="sr-only">Toggle navigation</span>
                             <span class="icon-bar"></span>
