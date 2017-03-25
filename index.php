9d412f4c04f2395f70d5e6ce6cd08a9c805b9384
Lorinda <lorinda@users.noreply.github.com>
2017-03-24 21:23:14 -0400

    Change site title to 'My Meals'

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
-$pageTitle = "My Blue Apron Meals";
+$pageTitle = "My Meals";
 $home = 'selected';
 include 'inc/header.php';
 ?>
+
 <div class="jumbotron well">
-    <h1>Welcome to My Blue Apron Meals</h1>
+    <h1>Welcome to My Meals</h1>
     <div class="description">
-        <p>Blue Apron is a service that delivers fresh ingredients and recipes to subscribers. I began using this service in January 2016.  I have collected a few of the recipes* I have cooked on this site searchable by ingredient and recipe name.  
+        <p>Blue Apron is a service that delivers fresh ingredients and recipes to subscribers. I began using this service in January 2016.  I have collected a few of the recipes* I have cooked on this site searchable by ingredient and recipe name along with a picture of the final result.
         </p>
         <p>*All recipes link to those listed on blueapron.com.
-        </p>
+        </p><br>
+        
     </div>
 </div>    
 
