75087f1d4a34acd7ac9344b4c1f88b45f2d13a4d
Lorinda <lorinda@users.noreply.github.com>
2017-03-25 18:19:06 -0400

    Shorten copyright text

--- inc/footer.php
+++ inc/footer.php
@@ -3,8 +3,7 @@
             <footer class="footer">
                 <p><?php echo "(c) ".date('Y')." My Meals by Lorinda Peoples"; ?></p>
                 <p class="blueapron"><i><?php echo "(c) Blue Apron is a registered trademark of Blue Apron, 
-                LLC which grants a limited right to create text hyperlinks to the Blue Apron 
-                site for noncommercial purposes."; ?></i></p>
+                LLC."; ?></i></p>
             </footer>
         </div>
     <!--jQuery -->
