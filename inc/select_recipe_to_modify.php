752b58d51d2573808557128ae0f830e78798f43a
Lorinda <lorinda@users.noreply.github.com>
2017-03-27 12:55:05 -0400

    Separate html code for modify-recipe.php

---
+++ inc/select_recipe_to_modify.php
@@ -0,0 +1,33 @@
+<div class="jumbotron well">
+    <div class="container">
+        <h1><?php echo "Update or Delete Recipe"; ?>
+        </h1>
+        <div class="row">
+            <div class="col-md-4">
+                <?php $allRecipes = getAllRecipeTitles(); ?>
+                <form name="form" class="">
+                    <label for="selectRecipe">Choose a Recipe:</label>
+                    <select name="selectRecipe" id="selectRecipe" onChange="go()">
+                         <option selected disabled>Choose here</option>
+                        <?php asort($allRecipes);
+                        foreach($allRecipes as $item){
+                            echo "<option value='modify-recipe.php?id=";
+                            echo $item['recipe_id'];
+                            echo "'>";
+                            echo $item['title'];
+                            echo "</option>";
+                        }
+                        ?>
+                    </select>
+                    <script type="text/javascript">
+                        function go(){
+                        location= document.form.selectRecipe.
+                        options[document.form.selectRecipe.selectedIndex].value
+                        }
+                    </script>
+                    <input type="submit" name="findRecipe" value="Load" onclick="go()" />
+                </form>
+            </div> <!--End "col-md-4" -->
+        </div><!--End "row" -->
+    </div><!-- End "container" -->
+</div><!--End "jumbotron" -->
