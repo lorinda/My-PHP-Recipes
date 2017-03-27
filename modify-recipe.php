482c977e956f0620abe6d308e8709e252e8afd29
Lorinda <lorinda@users.noreply.github.com>
2017-03-27 12:56:57 -0400

    Remove html page code to inc/select_recipe_to_modify.php and inc/modify_chosen_recipe.php

--- modify-recipe.php
+++ modify-recipe.php
@@ -85,136 +85,15 @@ if(isset($_POST['DELETE'])){
     }
     
 }
+//Display status message
 echo "<div class='status'><h2>".$status."</h2></div>";
 
 if(isset($_GET['id']) && isIDValid($_GET['id'])){
+////If Recipe chosen and valid display this page////
+    include 'inc/modify_chosen_recipe.php';
+    }else{
+    ////If No Recipe Chosen, display this page////
+    include 'inc/select_recipe_to_modify.php';
+    } //End else
 ?>
-<!---If Recipe chosen and valid display this page--->
-<div class="jumbotron well">
-    <div class="warning">
-        <form method='post' action=''>
-            <input type='submit' class='btn btn-danger' value='DELETE this Recipe' name='DELETE' />;
-        
-        </form>
-    <br><br>
-    </div>
-    <div class ="instructions">
-        <b>Modify this Recipe.</b> Fill in the associated textbox and click the link to rename Titles and Add Ingredients.  Click the link on the right of Ingredients to delete them from the recipe.
-    </div>
-    <h3><?php 
-        echo "<form method='post' action='' 
-                 onsubmit=\"return confirm('Are you sure you want to rename this recipe?');\">\n";
-        echo "<input type='text' value='" . $recipeTitle . "' name='title' size='40' />\n";
-        echo "<input type='hidden' value='". $id ."' name='id' />";
-        echo "<input type='submit' class='button--delete' value='[Rename Recipe Title]' />\n";
-        echo "</form>";
-        ?>
-    </h3>
-    <h4><?php
-        echo "<form method='post' action='' 
-                 onsubmit=\"return confirm('Rename recipe subtitle?');\">\n";
-        echo "<input type='text' value='" . $subTitle . "' name='subtitle' size='40' />\n";
-        echo "<input type='hidden' value='". $id ."' name='id' />";
-        echo "<input type='submit' class='button--delete' value='[Rename Recipe SubTitle]' />\n";
-        echo "</form>";
-        ?>
-    </h4>
-    <img src='<?php $image = getImage($id);
-              foreach($image as $item){ 
-                if($item != 'NULL'){
-                    echo $item;
-                }else{
-                    echo "/images/No_Image_Taken.jpg";
-                }
-              }
-              ?>' height="150" width="180">
-    <?php
-    if($image['img_src'] == "NULL"){
-        $image['img_src'] = "/images/No_Image_Taken.jpg";
-    }
-        echo "<form method='post' action='' 
-                 onsubmit=\"return confirm('Update the image?');\">\n";
-        echo "<input type='text' value='" . $image['img_src'] . "' name='image' size='40' />\n";
-        echo "<input type='hidden' value='". $id ."' name='id' />";
-        echo "<input type='submit' class='button--delete' value='[Choose New Image]' />\n";
-        echo "</form>";
-    ?>
-    <table>
-        <tr>
-        <th>Ingredients</th>
-        </tr>
-        <tr>
-           <td><?php
-            echo "<div class='col-md-2'><form method='post' action=''>";
-            echo "<input type='text' name='amount' value='1' name='amount' size='3' aria-describedby='helpAmount'/>";
-            echo "<span id='helpAmount' class='help-block'>Number</span></div>";
-
-            echo "<div class='col-md-2'><input type='text' value='cup' name='measurement' size='5' aria-describedby='helpMeasurement'/>";
-            echo "<span id='helpMeasurement' class='help-block'>Measure</span></div>";
-
-            echo "<div class='col-md-4'><input type='text' value='Ingredient' name='addIngredient' name='addIngredient' size='20' aria-describedby='helpIngredient'/>\n";
-            echo "<span id='helpIngredient' class='help-block'>Ingredient</span></div>";
-
-            echo "<div class='col-md-4'><input type='submit' class='button--delete' value='[Add this Ingredient]' />\n";
-            echo "</form></div>";
-            ?></td>
-        </tr>
-    </table>
-    <table>    
-        <?php 
-        $ingredients = getIngredients($id);
-        foreach ($ingredients as $item){
-            echo "<tr><td>";
-            echo "<form method='post' action='' 
-                 onsubmit=\"return confirm('Are you sure you want to delete this ingredient?');\">\n";
-            echo $item['amount']." ";
-            if($item['measurement'] != "NULL"){echo $item['measurement'];}
-            echo " ".$item['ingredient'];
-            echo "<input type='hidden' value='" . $item['ingredient'] . "' name='deleteIngredient' />\n";
-            echo "<input type='submit' class='button--delete' value='[Delete This Ingredient]' />\n";
-            echo "</form>";
-            echo "</td></tr>";
-        }
-        ?>
-    </table>
-    <br>
-</div>
-<?php 
-}else{
-?>
-<!--If No Recipe Chosen, display this page-->
-<div class="jumbotron well">
-    <div class="container">
-        <h1><?php echo "Update or Delete Recipe"; ?>
-        </h1>
-        <div class="row">
-            <div class="col-md-4">
-                <?php $allRecipes = getAllRecipeTitles(); ?>
-                <form name="form" class="">
-                    <label for="selectRecipe">Choose a Recipe:</label>
-                    <select name="selectRecipe" id="selectRecipe" onChange="go()">
-                         <option selected disabled>Choose here</option>
-                        <?php asort($allRecipes);
-                        foreach($allRecipes as $item){
-                            echo "<option value='modify-recipe.php?id=";
-                            echo $item['recipe_id'];
-                            echo "'>";
-                            echo $item['title'];
-                            echo "</option>";
-                        }
-                        ?>
-                    </select>
-                    <script type="text/javascript">
-                        function go(){
-                        location= document.form.selectRecipe.
-                        options[document.form.selectRecipe.selectedIndex].value
-                        }
-                    </script>
-                    <input type="submit" name="findRecipe" value="Load" onclick="go()" />
-                </form>
-            </div> <!--End "col-md-4" -->
-        </div><!--End "row" -->
-    </div><!-- End "container" -->
-</div><!--End "jumbotron" -->
-<?php } //End else?>
 <?php include 'inc/footer.php'; ?>
