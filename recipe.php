843d52ade853c992de11da24726c21fbbe4c7110
Lorinda <lorinda@users.noreply.github.com>
2017-03-28 21:22:35 -0400

    Remove display html and create included /inc files

--- recipe.php
+++ recipe.php
@@ -23,122 +23,12 @@ include 'inc/header.php';
 <?php
 if(isset($_GET['id']) && is_id_valid($db, $id)){
 //display recipe for id
-?>
-<div class="jumbotron well">
-    <div class="container">
-        <h1><?php echo $recipeTitle; ?></h1>
-        <h2><?php echo $subTitle; ?> </h2>
-        <div class="container">
-            <div class="col-md-4" style="padding-left: 0px;  padding-right: 0px;">
-                <img src='<?php
-                    $image = get_image($db, $id);
-                    foreach($image as $item){
-                        if($item != 'NULL' && $item !=""){
-                              echo $item;
-                        }else{
-                            echo "/images/No_Image_Taken.jpg";
-                        }
-                    }
-                ?>' alt="'Image of <?php echo $recipeTitle; ?>" class="img-responsive">
-            </div> <!--End col-md-4 div-->
-            <div class="col-md-4">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>Ingredients</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php 
-                        $ingredients = get_ingredients($db, $id);
-                        foreach ($ingredients as $item){
-                            echo "<tr><td>";
-                            echo $item['amount']." ";
-                            if ($item['measurement'] != "NULL"){
-                              echo $item['measurement']." ";  
-                            }
-                            echo $item['ingredient'];
-                            echo "</td></tr>";
-                        }
-                        ?>
-                    </tbody>
-                </table>
-            </div> <!--End col-md-4 div-->
-            <div class="col-md-4">
-                <br><b>Cooked on:</b>
-                <?php 
-                $dateURL = get_date_url($db, $id);
-                foreach ($dateURL as $item){
-                    echo $item['cooked_on'];
-                    echo "<br>";
-                    echo "<a href='".$item['url']."' target='_blank'>"."<h4>Link to Full Recipe on Blue Apron.com.</h4>"."</a>";
-                }
-                ?>
-                <br><br>
-                <a href="modify-recipe.php?id=<?php echo $id; ?>"><div class="button--delete">Update This Page Listing</div></a>
-            </div> <!-- End col-md-4 div-->
-        </div> <!-- End container div -->
-        <br>
-    </div><!-- End Recipe div -->
-</div><!-- End Jumbotron Well div -->
-<?php
-}//Close if(isset)
-//-----------else No ID set----------------//
+    include 'inc/view_chosen_recipe.php';
+}
 else{  
+//display selector page for recipes
+    include 'inc/select_recipe_to_view.php';
+} 
 ?>
-<div class="jumbotron well">
-    <div class="container">
-        <h1><?php 
-            echo "View Recipes"; 
-            ?>
-        </h1>
-        <div class="row">
-            <div class="col-md-4">
-                <?php $allRecipes = get_all_recipe_titles($db);
-                    asort($allRecipes); //Sort by title alphabetically
-                    $topRecipeInForm = array_values($allRecipes);//Choose top recipe for form redirection on pressing 'Load' button. All other recipes load when chosen.
-                ?>
-                <form name="form" method="post" action='<?php echo "recipe.php?id=".$topRecipeInForm[0]['recipe_id']; ?>'>
-                    <label for="selectRecipe">Choose a Recipe:</label>
-                    <select name="selectRecipe" id="selectRecipe" onChange="go()">
-                        <?php
-                        foreach($allRecipes as $item){
-                            echo "<option value='recipe.php?id=";
-                            echo $item['recipe_id'];
-                            echo "'>";
-                            echo $item['title'];
-                            echo "</option>";
-                        }
-                        ?>
-                    </select>
-                    <script type="text/javascript">
-                        //Function to redirect to <option> value page when recipe chosen
-                        function go(){
-                        location= document.form.selectRecipe.
-                        options[document.form.selectRecipe.selectedIndex].value
-                        }
-                    </script>
-                    <input type="submit" name="findRecipe" value="Load" />
-                </form>
-            </div> <!--End "col-md-4" -->
-        </div><!--End "row" -->
-        <h3>Or Choose a Random Recipe</h3>
-        <div class="row">
-             <ul>
-                <?php
-                $random = random_recipe($db);
-                foreach ($random as $item) {
-                    echo "<div class='col-md-4'>";
-                    echo "<a href='recipe.php?id=".$item['recipe_id']."'>";
-                    echo "<img src='".$item['img_src']."' alt='Image of Random Recipe' class='img-responsive'>";
-                    echo "</a></div>";
-                }
-                ?>							
-            </ul>
-        </div><!--End "row" -->
-    </div><!-- End "container" -->
-</div><!-- End "jumbotron well" -->
-
-<?php } //Close else ?>
 
 <?php include 'inc/footer.php'; ?>
