6d75d95bc6a22858684709faf6641fb4bb250a9d
Lorinda <lorinda@users.noreply.github.com>
2017-03-29 18:38:57 -0400

    Change include statements to require. Add comments.

--- modify-recipe.php
+++ modify-recipe.php
@@ -1,10 +1,13 @@
 <?php
+//To mark navigation option 'active'
 $modify_recipe = 'selected';
-include 'inc/functions.php';
-include 'inc/connection.php';
-include 'inc/header.php';
+
+require 'inc/functions.php';
+require 'inc/connection.php';
+require 'inc/header.php';
 $addIngredient = '';
 $status = '';
+
 //If id in url is in database
 if(isset($_GET['id']) && is_id_valid($db, $_GET['id'])){
     //Get id from url
@@ -20,13 +23,14 @@ if(isset($_GET['id']) && is_id_valid($db, $_GET['id'])){
 }else{
     //No valid recipe id
     $id = '';
-    //display update page
+    //display 'inc/select_recipe_to_modify.php' for invalid ID
 }
 
 //If Rename submission clicked for Title      
 if(isset($_POST['title'])){
     $title = trim(filter_input(INPUT_POST,'title',FILTER_SANITIZE_STRING));
     $title = ucwords($title);
+    //Write new Title to database
     if(set_title($db, $title, $id)){
         $recipeTitle = get_title($db, $id);
         $status = 'Title Updated';
@@ -34,10 +38,12 @@ if(isset($_POST['title'])){
         $status = 'Could not update Title';
     }    
 }
+
 //If Rename submission clicked for SubTitle     
 if(isset($_POST['subtitle'])){
     $sub = trim(filter_input(INPUT_POST,'subtitle',FILTER_SANITIZE_STRING));
     $sub = ucwords($sub);
+    //Write subtitle to database
     if(set_subtitle($db, $sub, $id)){
         $subTitle = get_subtitle($db, $id);
         $status = 'Subtitle Updated';
@@ -45,9 +51,11 @@ if(isset($_POST['subtitle'])){
         $status = 'Could not update SubTitle';
     }    
 }
+
 //If Choose New Image clicked
 if(isset($_POST['image'])){
     $img = trim(filter_input(INPUT_POST,'image',FILTER_SANITIZE_STRING));
+    //Write image filename to database
     if(set_image($db, $img, $id)){
         $status = 'Image Updated';
     }else{
@@ -60,6 +68,7 @@ if(isset($_POST['addIngredient'])){
     $amount = trim(filter_input(INPUT_POST,'amount',FILTER_SANITIZE_STRING));
     $measurement = trim(filter_input(INPUT_POST,'measurement',FILTER_SANITIZE_STRING));
     $addIngredient = ucwords($addIngredient);
+    //Write ingredient to database
     if(add_ingredient($db, $id, $addIngredient, $amount, $measurement)){
         $status = $addIngredient.' Added';
     }else{
@@ -69,6 +78,7 @@ if(isset($_POST['addIngredient'])){
 //If Delete Ingredient clicked
 if(isset($_POST['deleteIngredient'])){
     $ingredient = $_POST['deleteIngredient'];
+    //Delete ingredient from database
     if(delete_ingredient($db, $id,$ingredient)){
         $status = $ingredient.' Deleted';
     }else{
@@ -78,6 +88,7 @@ if(isset($_POST['deleteIngredient'])){
 
 //If Delete Recipe 
 if(isset($_POST['DELETE'])){
+    //Delete recipe from database
     if(delete_recipe($db, $id)){
         $status = 'Recipe Deleted';
     }else{
@@ -89,12 +100,14 @@ if(isset($_POST['DELETE'])){
 //If Rename Website Link
 if(isset($_POST['website'])){
     $url = trim(filter_input(INPUT_POST,'website',FILTER_SANITIZE_STRING));
+    //Write website link to database
     if(change_website($db, $id, $url)){
         $status = "Website changed";
     }else{
         $status = "Website could not be changed";
     }
 }
+
 //Display status message for any changes
 echo "<div class='status'><h2>".$status."</h2></div>";
 
@@ -104,10 +117,10 @@ echo "<div class='status'><h2>".$status."</h2></div>";
 
 if(isset($_GET['id']) && is_id_valid($db, $_GET['id'])){
 ////If Recipe chosen and valid ID, display this page////
-    include 'inc/modify_chosen_recipe.php';
+    require 'inc/modify_chosen_recipe.php';
     }else{
     ////If No Recipe Chosen, display this page////
-    include 'inc/select_recipe_to_modify.php';
-    } //End else
+    require 'inc/select_recipe_to_modify.php';
+    } 
 ?>
-<?php include 'inc/footer.php'; ?>
+<?php require 'inc/footer.php'; ?>
