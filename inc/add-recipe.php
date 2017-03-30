2fc887dbafea06e66eb1ec7c0f5f473fe6ef49a8
Lorinda <lorinda@users.noreply.github.com>
2017-03-30 19:16:41 -0400

    Add date and url input validation

--- inc/add-recipe.php
+++ inc/add-recipe.php
@@ -4,13 +4,13 @@
     <h1>Add a Recipe</h1>
     <form action="" method="post" class="form-horizontal">
         <div class="form-group">
-            <label class="control-label col-sm-2" for="title">Recipe Title (required)</label>
+            <label class="control-label col-sm-2" for="title">Recipe Title *required</label>
             <div class="col-sm-6">
                 <input type="text" class="form-control" name="title" id="title" />
             </div>
         </div>
         <div class="form-group">
-            <label class="control-label col-sm-2" for="subtitle">Recipe Subtitle (required)</label>
+            <label class="control-label col-sm-2" for="subtitle">Recipe Subtitle *required</label>
             <div class="col-sm-6">
                 <input type="text" class="form-control" name="subtitle" id="subtitle"  />
             </div>
@@ -18,7 +18,7 @@
         <div class="form-group">
             <label class="control-label col-sm-2" for="cooked_on">Date Cooked (MM/DD/YYYY)</label>
             <div class="col-sm-2">
-                <input type="text" class="form-control" name="cooked_on" id="cooked_on" />
+                <input type="text" class="form-control" pattern="(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](20)(1[6-7])" title="After 01/01/2016. Before 2018" name="cooked_on" id="cooked_on" />
             </div>
         </div>
         <br>
@@ -33,7 +33,7 @@
         <div class="form-group">
             <label class="control-label col-sm-2" for="url">Blue Apron Website Link</label>
             <div class="col-sm-6">
-                <input type="text" class="form-control" name="url" id="url" value="https://www.blueapron.com/recipes/"/>
+                <input type="url" pattern="https?://www.blueapron.com.+" class="form-control" name="url" id="url" value="https://www.blueapron.com/recipes/" title="Leave empty or begin with 'https://www.blueapron.com' . Thanks!"/>
             </div>
         </div>
         <br>
